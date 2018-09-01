40f142eb87f36c83fa451beb104e8211d4bafe57
Javier Machin <javier_machin_acosta@hotmail.com>
2018-09-01 18:00:17 +0100

    Set webpack to manage images

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 import './style.css';
+import Image from './ds3.jpg'
 
 document.getElementById("main-title").innerHTML = "Hello Webpack";
+
+// const content = document.getElementById("content");
+
+// const myImage = document.createElement("img");
+
+// myImage.src = Image;
+
+// content.appendChild(myImage);
